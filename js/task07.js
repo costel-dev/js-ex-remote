0d62482280b3c88ffee8a98ffff3aa18364ddf9f
costel-dev <costelcuconoiu@gmail.com>
2020-03-23 14:57:40 +0100

    task07 partial finished

--- js/task07.js
+++ js/task07.js
@@ -2,3 +2,11 @@
 7. Create an script that calculates the average number from an array
 
    - Numbers then can be added from prompt and new average given */
+
+function task07() {
+  let task07Array = [23, 34, 1, 56, 90];
+  let avg = task07Array.reduce(function(total, num) {
+    return (total + num) / task07Array.length;
+  });
+  alert(avg);
+}
